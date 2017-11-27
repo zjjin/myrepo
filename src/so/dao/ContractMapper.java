b65f9abf34b799836278b968a2df19e9d94d9365
周津津 <jinjin_z@gzleihou.cn>
2017-11-27 21:31:50 +0800

    aaa

---
+++ src/so/dao/ContractMapper.java
@@ -0,0 +1,17 @@
+package so.dao;
+
+import so.model.Contract;
+
+public interface ContractMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Contract record);
+
+    int insertSelective(Contract record);
+
+    Contract selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Contract record);
+
+    int updateByPrimaryKey(Contract record);
+}
